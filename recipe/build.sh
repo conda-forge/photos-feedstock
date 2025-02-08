fbc0c038a5ae70752f910405a4e7a8af54ac392c
Chris Burr <christopher.burr@cern.ch>
2025-02-08 13:25:25 +0100

    Add patch for linking HepMC(3)

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,6 @@ IFS=$'\n\t'
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* config/
 
-sed -i.bak -E 's@(libPhotosppHepMC3_la_LIBADD = libPhotospp.la)@\1 -lHepMC -lHepMC3@g' src/Makefile.in
-if diff src/Makefile.in.bak src/Makefile.in; then
-    echo "Failed to patch Makefile.in"
-    exit 1
-fi
-
 # Regenerate the configure script
 autoreconf -fvi
 
