982f3c044807753e52e3910cac25eca1a270caac
Chris Burr <christopher.burr@cern.ch>
2025-02-08 13:14:26 +0100

    WIP

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* config/
+
 autoconf
 ./configure --prefix="${PREFIX}" --with-hepmc="${PREFIX}" --with-hepmc3="${PREFIX}"
 # Yes, LD=CXX is intentional..
